25aaf284d7e8d2076589d135a8dbb0f8d734daf5
LuisBrodely <luisbrodely2@gmail.com>
2023-03-08 22:20:07 -0600

    Tipos de datos, id, input

---
+++ basic.py
@@ -0,0 +1,34 @@
+# Imprimir en Python
+print('Hello World :D')
+
+# Declaracion de variables
+variable = 'Esto es una variable de tipo String'
+print(variable)
+
+# Direccion de memoria de una variable
+print(id(variable))
+
+# Tipos de datos
+
+# <class 'int'>
+numero = 10
+print(type(numero))
+
+# <class 'floeat'>
+flotante = 10.2
+print(type(flotante))
+
+# <class 'str'>
+cadena = 'Hola XD'
+print(cadena)
+
+# <class 'bool'>
+booleano = True
+print(type(booleano))
+
+# Ingresar datos desde consola input
+dato = input('Ingresa como estuvo tu dia: ')
+print('Tu dia estuvo:', dato)
+
+
+
